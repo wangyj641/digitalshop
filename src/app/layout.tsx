36e4a72f16f22f8c81c8bd2d54523344dc70dcfd
Yongjun <wangyj641@gmail.com>
2023-12-14 20:54:07 +0800

    Implement home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn("relative h-full font-sans antialiased", inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
-          {children}
+          <div className='flex-grow flex-1'>
+            {children}
+          </div>
+
         </main>
       </body>
     </html>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+            Navbar
+        </div>
+    )
+}
