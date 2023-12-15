2d0218b3ee3c7ff97ddd8b8ee0f679a71fc1b905
Yongjun <wangyj641@gmail.com>
2023-12-15 23:33:59 +0800

    Add NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import Link from 'next/link'
+import { Icons } from './ui/Icons'
 
 export default function Navbar() {
     return (
         <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
-            Navbar
+            <header className='relative bg-white'>
+                <MaxWidthWrapper>
+                    <div className='border-b border-gray-200'>
+                        <div className='flex h-16 items-center'>
+                            {/* TODO: Mobile nav*/}
+                            <div className='ml-4 flex lg:ml-0'>
+                                <Link href='/'>
+                                    <Icons.logo className='h-10 w-10' />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
+            </header>
         </div>
     )
 }
---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,12 @@
+import { LucideProps } from 'lucide-react'
+import React from 'react'
+
+export const Icons = {
+    logo: (props: LucideProps) => (
+        <svg {...props} viewBox='0 0 512 512'>
+
+        </svg>
+
+    )
+
+}
